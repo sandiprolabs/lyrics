ff58f35835e7d4d6677b8b1bbc7f5c6e547bd271
Catalina <catalina@MacBook-Pro-Catalina.local>
2025-11-28 18:45:00 +0700

    initial commit

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../prisma/db/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({
