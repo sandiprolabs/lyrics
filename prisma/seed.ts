1668a028a22c46bc4122cc0a919885a7fb02e954
Catalina <catalina@MacBook-Pro-Catalina.local>
2025-11-28 18:46:34 +0700

    initial commit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../prisma/db/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import * as bcrypt from 'bcryptjs'
 
